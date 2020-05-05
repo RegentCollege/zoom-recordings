b88c6d1f0d190aa6ddda0a42e2615d1f7803dba0
Cam <ctucker@regent-college.edu>
2020-05-05 13:57:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,11 +7,15 @@ env=${APP_ENV:-production}
 
 if [ "$env" != "local" ]; then
     echo "Caching configuration..."
-    (cd /var/www/zoom-recordings/current && php artisan config:cache && php artisan view:cache)
+    [ -d "/var/www/zoom-recordings/current" ] && (cd /var/www/zoom-recordings/current && php artisan config:cache && php artisan view:cache)
 fi
 
 if [ "$role" = "app" ]; then
 
     exec apache2-foreground
+    
+elif [ "$role" = "queue" ]; then
+
+    [ -d "/var/www/zoom-recordings/current" ] && (cd /var/www/zoom-recordings/current && php artisan queue:work)
 
 fi
